6702b60b4df237b1b0b6b10c64210803a79da3e7
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2021-03-12 15:27:06 +0100

    URL links by the name of the routes

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,22 +13,4 @@ def entry(request, title):
         "title": title,
         "entry": util.get_entry(title)
     })
-
-#def search(request):
-
-
-def add(request):   
-    if request.method == "POST":
-        form = NewTaskForm(request.POST)
-        if form.is_valid():
-            task = form.cleaned_data["task"]
-            request.session["tasks"] += [task]
-            return HttpResponseRedirect(reverse("tasks:index"))
-        else:
-            return render(request, "tasks/add.html", {
-                "form": form
-            })
-    
-    return render(request, "tasks/add.html", {
-            "form": NewTaskForm()
-        })    
+   
