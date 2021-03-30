7f5d169557d5ba09c4252ecd27a7836719037f9a
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2021-03-30 21:36:01 +0100

    markdown import

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from . import util
+import markdown
 
 class NewEntryForm(forms.Form):
     title = forms.CharField(label="New Title")
